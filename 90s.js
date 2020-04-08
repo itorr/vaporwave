35841de8e30c3c9b98a3c50ce86b63b3777f9a3b
itorr <itorrrrrr@me.com>
2020-04-08 12:15:33 +0800

    画面亮度、画面反差、暗部褪色、亮部褪色、紫鲜艳度、青鲜艳度、紫色偏移、青色偏移、像素合并、色彩断层、横向偏移、纵向偏移、整倍偏移、隔行扫描、信息置换、边缘偏差、信号质量、信号噪声、胶片颗粒 实现

---
+++ 90s.js
@@ -0,0 +1,404 @@
+const rgb2yuv = (r,g,b)=>{
+	var y, u, v;
+
+	y = r *  .299000 + g *  .587000 + b *  .114000;
+	u = r * -.168736 + g * -.331264 + b *  .500000 + 128;
+	v = r *  .500000 + g * -.418688 + b * -.081312 + 128;
+
+	y = Math.floor(y);
+	u = Math.floor(u);
+	v = Math.floor(v);
+
+	return [y,u,v];
+};
+
+const yuv2rgb = (y,u,v)=>{
+	var r,g,b;
+
+	r = y + 1.4075 * (v - 128);
+	g = y - 0.3455 * (u - 128) - (0.7169 * (v - 128));
+	b = y + 1.7790 * (u - 128);
+
+	r = Math.floor(r);
+	g = Math.floor(g);
+	b = Math.floor(b);
+
+	r = (r < 0) ? 0 : r;
+	r = (r > 255) ? 255 : r;
+
+	g = (g < 0) ? 0 : g;
+	g = (g > 255) ? 255 : g;
+
+	b = (b < 0) ? 0 : b;
+	b = (b > 255) ? 255 : b;
+
+	return [r,g,b];
+};
+
+const randRange =(a,b)=>Math.floor(Math.random()*(b-a)+a);
+
+
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+
+
+let width  = 640;
+let height = 480;
+let scale  = width / height;
+
+
+
+let lastShowPixel = null;
+let lastConfigString = null;
+const reDraw = (img,config,callback)=>{
+	if(!img || !config) return;
+
+	const configString = JSON.stringify(config)+img.src;
+
+	if(lastConfigString === configString)return;
+
+	lastConfigString = configString;
+
+	const oriWidth  = img.naturalWidth;
+	const oriHeight = img.naturalHeight;
+
+	let oriScale = oriWidth / oriHeight;
+
+
+	console.log(oriWidth, oriHeight);
+
+
+	if(!config.sizeOrigin){
+		width  = 640;
+		height = 480;
+	}else{
+		width  = oriWidth;
+		height = oriHeight;
+		if(width > 1920){
+			height = 1920 / width * height;
+			width = 1920;
+		}
+	}
+
+	width  = Math.floor( width  / config.zoom );
+	height = Math.floor( height / config.zoom );
+
+	scale = width / height;
+
+
+	canvas.width = width;
+	canvas.height = height;
+
+	let cutLeft = 0;
+	let cutTop  = 0;
+
+	let calcWidth = oriWidth;
+	let calcHeight = oriHeight;
+
+
+	let setLeft = 0;
+	let setTop  = 0;
+
+	let setWidth = width;
+	let setHeight = height;
+
+
+	if(config.fit === 'cover'){
+		if(oriScale > scale){
+			cutLeft = ( oriWidth - oriHeight * scale )/2;
+			calcWidth = oriHeight * scale;
+			calcHeight = oriHeight;
+		}else{
+			cutTop  = ( oriHeight - oriWidth / scale )/2;
+			calcWidth = oriWidth;
+			calcHeight = oriWidth / scale;
+		}
+
+	}else if(config.fit === 'contain'){
+		if(oriScale > scale){ //原图更宽
+			setWidth  = width;
+			setHeight = width / oriScale;
+			setTop  = ( height - setHeight )/2;
+		}else{
+			setWidth  = height * oriScale;
+			setHeight = height;
+			setLeft = ( width - setWidth )/2;
+		}
+	}else{
+
+	}
+
+
+
+	console.log(cutLeft,cutTop);
+	console.log(calcWidth,calcHeight);
+	console.log(setLeft,setTop);
+
+
+
+	if(config.border){
+		setLeft += config.border;
+		setTop  += config.border;
+		setWidth  -= config.border *2;
+		setHeight -= config.border *2;
+	}
+
+
+
+	ctx.drawImage(
+		img,
+		cutLeft,cutTop,
+		calcWidth,calcHeight,
+
+		setLeft,setTop,
+		setWidth,setHeight
+	);
+
+	if(config.sharpen)
+		sharpen();
+	// return;
+
+	let pixel = getPixels();
+
+	let pixelData = pixel.data;
+
+
+	let shiftUPixel = 4 * config.shiftX + 4 * width * config.shiftY;
+	let shiftVPixel = 4 * (config.shiftX + config.shiftY);
+
+
+	if(config.snow){
+		// // console.log(lastShowPixel,lastShowPixel&&lastShowPixel.width,pixel.width);
+		// if(lastShowPixel && lastShowPixel.width === pixel.width){
+		// 	pixel     = lastShowPixel;
+		// 	pixelData = lastShowPixel.data;
+		// }else{
+
+			pixel = ctx.createImageData(pixel.width, pixel.height);
+			pixelData = pixel.data;
+
+			const linex = 2 * 4 * width;
+
+			for(let i = 0;i < pixelData.length;i += 4){
+				pixelData[i                 ] = randRange(0,25)*10;//Math.floor(i / (config.light+1)/1000 % 200) + randRange(0,55);
+				pixelData[i+1 - shiftUPixel ] = randRange(108,148);//randRange(68,188);
+				pixelData[i+2 - shiftVPixel ] = randRange(118,138);
+				pixelData[i+3 - shiftVPixel ] = 255;
+
+				if(Math.floor(i/4/width)%2 === 0){
+					pixelData[i - shiftUPixel - linex] = pixelData[i - shiftUPixel ] + 100;
+				}
+			}
+		// }
+
+	}else{
+		for(let i = 0;i < pixelData.length;i += 4){
+			let yuv = rgb2yuv(
+				pixelData[i  ],
+				pixelData[i+1],
+				pixelData[i+2],
+			);
+
+			// UV 漂移
+			yuv = UVshifting(yuv,config);
+
+			pixelData[i                 ] = yuv[0];
+			pixelData[i+1 - shiftUPixel ] = yuv[1];
+			pixelData[i+2 - shiftVPixel ] = yuv[2];
+
+		}
+	}
+
+	if(config.yuv420){
+		// 420 压缩
+		const p = 4;
+		const s = Math.sqrt(p);
+		let maxX = Math.floor(width/s);
+		let maxY = Math.floor(height/s);
+		const lineP = Math.floor(config.yuv420Noise * 4 * width); //一排像素数
+		const pixelLength= pixelData.length;
+
+		for(let x=0;x < maxX;x++){
+			for(let y=0;y < maxY;y++){
+				const i = y * s * 4 * width + x * s * 4;
+
+				// for(let si=0;si<s;si++){
+				//
+				// }
+				pixelData[(i + 1)%pixelLength] =
+				pixelData[(i + 1 + 4)%pixelLength] =
+				pixelData[(i + 1 + lineP)%pixelLength] =
+				pixelData[(i + 1 + lineP + 4)%pixelLength] = (
+					pixelData[(i + 1)%pixelLength] +
+					pixelData[(i + 1 + 4)%pixelLength] +
+					pixelData[(i + 1 + lineP)%pixelLength] +
+					pixelData[(i + 1 + lineP + 4)%pixelLength]
+				)/p;
+
+
+				pixelData[(i + 2)%pixelLength] =
+				pixelData[(i + 2 + 4)%pixelLength] =
+				pixelData[(i + 2 + lineP)%pixelLength] =
+				pixelData[(i + 2 + lineP + 4)%pixelLength] = (
+					pixelData[(i + 2)%pixelLength] +
+					pixelData[(i + 2 + 4)%pixelLength] +
+					pixelData[(i + 2 + lineP)%pixelLength] +
+					pixelData[(i + 2 + lineP + 4)%pixelLength]
+				)/p;
+			}
+		}
+
+	}
+
+
+	if(config.interlaced) {
+		// // console.log(lastShowPixel,lastShowPixel&&lastShowPixel.width,pixel.width);
+		// if(lastShowPixel && lastShowPixel.width === pixel.width){
+		// 	pixel     = lastShowPixel;
+		// 	pixelData = lastShowPixel.data;
+		// }else{
+
+		const linex = 2 * 4 * width;
+
+		for (let i = 0; i < pixelData.length; i += 4) {
+
+			if (Math.floor(i / 4 / width) % 2 === 0) {
+				pixelData[i - linex - config.interlaced*4] = pixelData[i - linex];
+			}
+		}
+	}
+
+	if(config.replace) {
+		for (let i = 0; i < pixelData.length; i ++) {
+			pixelData[i - config.replace] = pixelData[i]
+		}
+	}
+
+	if(config.invertLight){
+		for (let i = 0; i < pixelData.length; i +=4) {
+			pixelData[i] = 255 - pixelData[i]
+		}
+	}
+
+	if(config.lightNoise){
+		let halt = config.lightNoise/2;
+		for (let i = 0; i < pixelData.length; i +=4) {
+			pixelData[i] = pixelData[i] + (randRange(0,config.lightNoise) - halt)// * (255 - pixelData[i])/255;
+		}
+	}
+	if(config.darkNoise){
+		let halt = config.darkNoise/2;
+		for (let i = 0; i < pixelData.length; i +=4) {
+			pixelData[i] = pixelData[i] + (randRange(0,config.darkNoise) - halt) * (255 - pixelData[i])/255;
+			//噪声在亮部不那么明显
+		}
+	}
+
+
+	for(let i = 0;i < pixelData.length;i += 4){
+
+		let _rgb = yuv2rgb(
+			pixelData[i],
+			pixelData[i+1],
+			pixelData[i+2],
+		);
+
+		pixelData[i   ] = _rgb[0];
+		pixelData[i+1 ] = _rgb[1];
+		pixelData[i+2 ] = _rgb[2];
+	}
+
+	// blurC();
+	ctx.putImageData(pixel,0,0);
+
+
+	canvas.toBlob(blob=>{
+		// console.log(blob);
+		// blob = blob.slice(0,blob.size-config.damage);
+		callback(URL.createObjectURL(blob));
+
+	},'image/jpeg',config.quality);
+
+	// callback(canvas.toDataURL('image/jpeg',config.quality));
+};
+
+
+const UVshifting = (yuv,config)=>{
+
+	const levelLower = num=>Math.round(num / config.level) * config.level;
+
+	//对比度
+	yuv[0] = ((yuv[0] - 128)*config.contrast + 128);
+
+	//亮度
+	yuv[0] = yuv[0] * config.light;
+
+	yuv[0] = Math.max(yuv[0],config.darkFade);
+	yuv[0] = Math.min(yuv[0],255-config.brightFade);
+
+
+	yuv[1] = Math.min(255, (yuv[1]-128) * config.vividU + config.shiftU + 128 );
+	yuv[2] = Math.min(255, (yuv[2]-128) * config.vividV + config.shiftV + 128 );
+
+	yuv[1] = levelLower(yuv[1]);
+	yuv[2] = levelLower(yuv[2]);
+	return yuv;
+};
+
+
+
+const getPixels = function() {
+	return ctx.getImageData(0,0,width,height);
+};
+
+const convolute = function(pixels, weights) {
+	var side = Math.round(Math.sqrt(weights.length));
+	var halfSide = Math.floor(side/2);
+
+	var src = pixels.data;
+	var sw = pixels.width;
+	var sh = pixels.height;
+
+	var w = sw;
+	var h = sh;
+	var output = ctx.createImageData(w, h);
+	var dst = output.data;
+
+
+	for (var y=0; y<h; y++) {
+		for (var x=0; x<w; x++) {
+			var sy = y;
+			var sx = x;
+			var dstOff = (y*w+x)*4;
+			var r=0, g=0, b=0;
+			for (var cy=0; cy<side; cy++) {
+				for (var cx=0; cx<side; cx++) {
+					var scy = Math.min(sh-1, Math.max(0, sy + cy - halfSide));
+					var scx = Math.min(sw-1, Math.max(0, sx + cx - halfSide));
+					var srcOff = (scy*sw+scx)*4;
+					var wt = weights[cy*side+cx];
+					r += src[srcOff]   * wt;
+					g += src[srcOff+1] * wt;
+					b += src[srcOff+2] * wt;
+				}
+			}
+			dst[dstOff] = r;
+			dst[dstOff+1] = g;
+			dst[dstOff+2] = b;
+			dst[dstOff+3] = 255;
+		}
+	}
+	return output;
+};
+
+
+const sharpen = function() {
+	ctx.putImageData(convolute(
+		getPixels(),
+		[
+			0, -1,  0,
+			-1, 2,  2,
+			0, -1,  0
+		]
+	), 0, 0);
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+
+
+https://www.html5rocks.com/en/tutorials/canvas/imagefilters/
+https://en.wikipedia.org/wiki/YUV
+https://en.wikipedia.org/wiki/Convolution
